4f3334ad4cd4e3d06eeed572a65fc2ae8dfda222
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-05-29 15:30:48 +0530

    Add files via upload

---
+++ Objects_of_objects.js
@@ -0,0 +1,14 @@
+const classInfo = {
+  adi: {
+    grade: "A+",
+    city: "Delhi",
+  },
+  disha: {
+    grade: "A",
+    city: "Pune",
+  },
+  karan: {
+    grade: "0",
+    city: "Mumbai",
+  },
+};
