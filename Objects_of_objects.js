566fbd810226bb1576056b4f9f4b767b68fc36fb
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-05-29 15:34:53 +0530

    Objects_of_objects.js

--- Objects_of_objects.js
+++ Objects_of_objects.js
@@ -12,3 +12,21 @@ const classInfo = {
     city: "Mumbai",
   },
 };
+
+------------------------------------------------------------------------------------------
+
+  OUTPUT:
+
+classInfo
+{adi: {…}, disha: {…}, karan: {…}}
+adi: {grade: 'A+', city: 'Delhi'}
+disha: {grade: 'A', city: 'Pune'}
+karan: {grade: '0', city: 'Mumbai'}
+
+classInfo.adi
+{grade: 'A+', city: 'Delhi'}
+    city: "Delhi"
+    grade: "A+"
+
+classInfo.disha.city
+'Pune'
