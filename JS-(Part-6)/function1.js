1da92511a518d6dcc11fca01c682b1fddbe11447
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-06-04 12:46:00 +0530

    Add files via upload

---
+++ JS-(Part-6)/function1.js
@@ -0,0 +1,29 @@
+function hello() {
+  console.log("HELLO!!");
+}
+
+hello();
+hello();
+
+function printName() {
+  console.log("Deeksha Sharma");
+}
+printName();
+
+function print1to5() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
+print1to5();
+
+function isadult() {
+  let age = 18;
+  if (age >= 18) {
+    console.log("Adult");
+  } else {
+    console.log("Not Adult");
+  }
+}
+
+isadult();
