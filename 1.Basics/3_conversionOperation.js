7d44f37fdb55d00cc05f565a1106cd40cc61e0b9
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-10-30 05:14:19 +0000

    conversion of Data types

---
+++ 1.Basics/3_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let login =1
+let booleanLogin = Boolean(login)
+console.log(booleanLogin);
+
+// 1 => true; false => 0
+// " " => false
+// "deeksha" => true
+
+
+let SomeNum = 33
+let stringNum = String(SomeNum)
+console.log(stringNum);
+console.log(typeof stringNum);
