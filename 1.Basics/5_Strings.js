4b7738c51cac5dd1b4aca76d1865277169dd63a8
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-10-30 13:05:38 +0000

    strings

---
+++ 1.Basics/5_Strings.js
@@ -0,0 +1,11 @@
+const name = "deeksha"
+const repoCount = 20
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello, My name is ${name} and My Repo Count is ${repoCount}`);
+
+const gameName = new String('jyoti');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
