26a0863e2400dff5a8e5b58b96efcfb5416881bd
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-10-30 14:57:37 +0000

    String

--- 1.Basics/5_Strings.js
+++ 1.Basics/5_Strings.js
@@ -7,5 +7,16 @@ console.log(`Hello, My name is ${name} and My Repo Count is ${repoCount}`);
 
 const gameName = new String('jyoti');
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2)); 
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-4,2);
+console.log(anotherString);
