32103436a41593f3a94d577df58d697be732856c
Deeksha Sharma <138354906+DeekshaSharma7@users.noreply.github.com>
2024-11-02 09:50:03 +0000

    arrays2

--- 2.Basics/2_arrays.js
+++ 2.Basics/2_arrays.js
@@ -1,10 +1,31 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+ //marvel_heros.push(dc_heros)
 
-console.log(marvel_heros);
-console.log(marvel_heros[3][1]);
+// console.log(marvel_heros);
+//  console.log(marvel_heros[3][1]);
 
-const allHeros = marvel_heros.concat(dc_heros)
-console.log(allHeros);
+// const allHeroes = marvel_heros.concat(dc_heros);
+// console.log(allHeroes);
+
+// ************ Spread *****
+
+const all_new_heroes = [...marvel_heros, ...dc_heros];
+console.log(all_new_heroes);
+
+const anotherArr = [1,2,2,[4,5,6], 7, [6,7,[4,5]]]
+const real_another_arr = anotherArr.flat(Infinity);
+console.log(real_another_arr);
+
+
+console.log(Array.isArray("Deeksha"));
+console.log(Array.from("Deeksha"));
+
+console.log(Array.from({name:"deeksha"})) //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
